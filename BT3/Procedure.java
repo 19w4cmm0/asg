29aa7d09d8e2a7cbf0b0fd33c2537bf9f34b6784
19w4cmm0 <125661366+19w4cmm0@users.noreply.github.com>
2023-02-25 20:25:17 +0700

    Add files via upload

---
+++ BT3/Procedure.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BT3;
+
+/**
+ *
+ * @author Admin
+ */
+import java.util.Scanner;
+public class Procedure {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float a;
+        System.out.println("Nhap luong tang: ");
+        a = scanner.nextFloat();
+        NhanVien nv = new NhanVien();
+        nv.setTenNhanVien("Nguyen Van A");
+        System.out.println("Ten Nhan Vien: " +nv.getTenNhanVien());
+        nv.setLuongCoBan(5000);
+        nv.setHeSoLuong(2);
+        float luong=(float) ((float) nv.getHeSoLuong()*nv.getLuongCoBan());
+        System.out.println("Luong:" +luong);
+        nv.setHeSoLuong(a);
+        double tangluong=((double) nv.getHeSoLuong()*nv.getLuongCoBan());
+         if(tangluong>nv.getLuongMax())
+         {
+            System.out.println("False");
+            
+         }
+         else{
+             System.out.println("Luong tang: "+tangluong);}
+         
+             
+        
+        
+        
+    }
+}
